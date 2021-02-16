dc9eb1f619f93edf383bf53765beb7babd83c1ef
jgnogueira <josegmnogueira@hotmail.com>
2021-02-16 20:18:57 -0300

    Configurando arquivo de testes

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:bytebank_armazenamento_interno/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Bytebank());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
